141c5f1ae12a6261c1e0a0353b3de54e29fc1628
dEagleProg <deagle.websites@gmail.com>
2025-03-28 19:36:01 +0100

    edit music

--- src/App.tsx
+++ src/App.tsx
@@ -434,10 +434,6 @@ function App() {
       setCurrentPhase(0);
       setKey(0);
     } else {
-      if (audioRef.current) {
-        audioRef.current.pause();
-        audioRef.current.currentTime = 0;
-      }
       setPreparationStage('initial');
       setIsRunning(false);
       setCountdownNumber(3);
